79fc7e259b09de554a12aaa76d3de1cd73f209b7
antoine.roux@zenika.com <antoinroux@hotmail.fr>
2019-03-11 23:27:59 +0100

    Destroy spring boot configuration
    use Manual configuration instead

--- OAuth2/src/main/java/com/example/OAuth2/configurations/WebSecurityConfiguration.java
+++ OAuth2/src/main/java/com/example/OAuth2/configurations/WebSecurityConfiguration.java
@@ -1,14 +1,34 @@
 package com.example.OAuth2.configurations;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2ClientContext oauth2ClientContext;
+
+    @Autowired
+    public WebSecurityConfiguration(OAuth2ClientContext oauth2ClientContext) {
+        this.oauth2ClientContext = oauth2ClientContext;
+    }
 
 //    private final AuthenticationManager authenticationManager;
 //
@@ -26,9 +46,56 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
             .anyRequest()
                 .authenticated()
+            .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
+                .csrf().disable()
+        ;
+    }
 
-            .and().logout().logoutSuccessUrl("/").permitAll()
-            .and().csrf().disable();
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter =
+                new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+        tokenServices.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(tokenServices);
+        return facebookFilter;
+    }
+
+    /**
+     * authorization code resource
+     * @return
+     */
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    /**
+     * resource server
+     * @return
+     */
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
+
+    /**
+     * redirect filter to oauth facebook login page
+     * @param filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
     }
 
 //    @Override
